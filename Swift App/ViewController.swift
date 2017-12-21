b9eaebddf7e80fe3fa28cea507beee072e731df3
Hassan Govani <hasangovani1996@gmail.com>
2017-12-21 17:17:44 -0500

    Version 1.0

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func button1(_ sender: Any) {
+        
+            tapCount = tapCount + 1
+        
+            label.text = "You tapped the button \(tapCount) times now"
+        
+        
+        
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        label.text = "Hello World"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
